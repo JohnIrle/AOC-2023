139cb1e2d4824ceb36a52f66c9e579696779a11d
John Irle <irlejohn@gmail.com>
2023-12-02 02:43:31 -0600

    feat: refactor day to use map and chain iterators

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,47 +1,73 @@
 use std::fs::read_to_string;
 
-const RED_THRESHOLD:u32 = 12;
-const GREEN_THRESHOLD:u32 = 13;
-const BLUE_THRESHOLD:u32 = 14;
+const RED_THRESHOLD: u32 = 12;
+const GREEN_THRESHOLD: u32 = 13;
+const BLUE_THRESHOLD: u32 = 14;
 
 fn main() {
     let input = read_to_string("./day_02/input_part_1.txt").expect("Could Not read file");
 
-    let total = get_total(&input);
+    let part1_total = get_total(&input);
+    let part2_total = get_power_total(&input);
 
-    println!("Part one: {}", total);
+    println!("Part one: {}", part1_total);
+    println!("Part two: {}", part2_total);
 }
 
 fn get_total(input: &str) -> u32 {
-    let mut total = 0;
-
-    for line in input.lines() {
-        let game = line.split(':').collect::<Vec<&str>>()[0];
-        let number = game.trim().split(' ').collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
-        let rest = line.split(':').collect::<Vec<&str>>()[1];
-        let sets = rest.split(';').collect::<Vec<&str>>().join(",");
-        let sets_over_threshold = sets.split(',').filter(|s| is_over_threshold(s)).collect::<Vec<&str>>();
-        if sets_over_threshold.is_empty() {
-            total += number;
+    input.lines().enumerate().map(|(index, line)| {
+        let game_number = index + 1;
+        let cubes = parse_cubes(line);
+        if cubes.iter().all(|c| !is_over_threshold(*c)) {
+            return game_number as u32
         }
-    }
-    total
+        0
+    }).sum::<u32>()
 }
 
-
-fn is_over_threshold(item: &str) -> bool {
-    let number_color = item.trim().split(' ').collect::<Vec<&str>>();
-    let number = number_color[0];
-    let parsed_number = number.parse::<u32>().unwrap();
-    let color = number_color[1];
+fn is_over_threshold((qty, color): (u32, &str)) -> bool {
     match color {
-        "green" => parsed_number > GREEN_THRESHOLD,
-        "blue" => parsed_number > BLUE_THRESHOLD,
-        "red" => parsed_number > RED_THRESHOLD,
+        "green" => qty > GREEN_THRESHOLD,
+        "blue" => qty > BLUE_THRESHOLD,
+        "red" => qty > RED_THRESHOLD,
         _ => unreachable!(),
     }
 }
 
+fn parse_cubes(line: &str) -> Vec<(u32, &str)> {
+    let game = line.split(": ").collect::<Vec<&str>>()[1];
+    let sets = game.split("; ");
+    let cubes = sets
+        .flat_map(|s| s.split(", "))
+        .filter_map(|c| {
+            let mut parts = c.split(' ');
+            let qty = parts.next().map(|q| q.parse::<u32>().unwrap());
+            let color = parts.next();
+            match (qty, color) {
+                (Some(qty), Some(color)) => Some((qty, color)),
+                _ => None
+            }
+        });
+    cubes.collect()
+}
+
+fn get_power_total(input: &str) -> u32 {
+    input.lines().map(|line| {
+        let cubes = parse_cubes(line);
+        let mut max_green = 0;
+        let mut max_blue = 0;
+        let mut max_red = 0;
+        for (qty, color) in cubes {
+            match color {
+                "red" => if qty > max_red {max_red = qty}
+                "green" => if qty > max_green { max_green = qty}
+                "blue" => if qty > max_blue { max_blue = qty }
+                _ => unreachable!()
+            }
+        }
+        max_green * max_blue * max_red
+    }).sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -66,6 +92,6 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 "#;
-        // assert_eq!(get_total(input), 2286)
+        assert_eq!(get_power_total(input), 2286)
     }
 }
