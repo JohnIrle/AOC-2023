a4fb8aa672789112ecc5ab83b7aa4368f9fc4d28
John Irle <irlejohn@gmail.com>
2023-12-01 20:36:39 -0600

    feat: part one and part two

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,109 +1,66 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
 fn main() {
-    let part1_input = read_to_string("./day_01/input_part_1.txt").expect("Could Not read file");
-    let total: u32 = part1_input.lines().map(|l| parse_line(l.to_owned())).sum();
-    println!("part 1 solution: {}", total);
+    let input = read_to_string("./day_01/input_part_1.txt").expect("Could Not read file");
+    let sum_1: u32 = input.lines().map(|l| parse_line(l, Regex::new(r"[0-9]").unwrap())).sum();
+    let sum_2: u32 = input.lines().map(|l| parse_line(l, Regex::new(r"[0-9]|one|two|three|four|five|six|seven|eight|nine").unwrap())).sum();
 
-    let total: u32 = part1_input.lines().map(parse_numbers).map(parse_line).sum();
-    println!("part 2 solution: {}", total);
+    println!("part 1 solution: {}", sum_1);
+    println!("part 2 solution: {}", sum_2);
 }
 
-fn parse_numbers(input: &str) -> String {
-    let index_values = [("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")];
+fn parse_line(line: &str, regex: Regex) -> u32 {
+    let words = parse_words(line, regex);
 
-    let mut results = index_values
-        .iter()
-        .filter_map(
-            |(word, value)| input.find(word).map(
-                |index| (index, *value, *word))
-        )
-        .collect::<Vec<(usize, &str, &str)>>();
-    results.sort_by(|(a, _, _), (b, _, _)| a.partial_cmp(b).unwrap());
+    let numbers: Vec<u32> = words.iter().map(|w| word_to_value(w)).collect();
 
-    let mut output = input.to_string();
-    for (_, value, word) in results {
-        output = output.replace(word, value);
-    }
-    output
+    let first = numbers.first().unwrap_or(&0u32);
+    let last = numbers.last().unwrap_or(&0u32);
+
+    first * 10 + last
 }
 
-fn parse_line(input: String) -> u32 {
-    let mut first: Option<u32> = None;
-    let mut last: Option<u32> = None;
-
-    for place in input.chars() {
-        if place.is_numeric() {
-            match first {
-                None => {
-                    first = place.to_digit(10);
-                    last = first;
-                }
-                Some(_) => last = place.to_digit(10)
-            }
+fn parse_words(line: &str, re: Regex) -> Vec<&str> {
+    let mut words: Vec<&str> = Vec::new();
+    for i in 0..line.len() {
+        if let Some(word)= re.find(&line[i..]) {
+            words.push(word.as_str())
         }
     }
+    words
+}
 
-
-    match (first, last) {
-        (Some(x), Some(y)) => x * 10 + y,
-        _ => 0
+fn word_to_value(word: &str) -> u32 {
+    match word {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        x => x.parse::<u32>().unwrap()
     }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn test_parse_line() {
-    //     let input = "1abc2";
-    //     let output = parse_line(input);
-    //
-    //     assert_eq!(output, 12);
-    //
-    //     let input = "pqr3stu8vwx";
-    //     let output = parse_line(input);
-    //     assert_eq!(output, 38);
-    //
-    //     let input = "a1b2c3d4e5f";
-    //     let output = parse_line(input);
-    //     assert_eq!(output, 15);
-    //
-    //     let input = "treb7uchet";
-    //     let output = parse_line(input);
-    //     assert_eq!(output, 77)
-    // }
 
     #[test]
-    fn test_parse_numbers() {
-        let input = "two1nine";
-        let output = "219";
-        assert_eq!(parse_numbers(input), output);
+    fn test_part_1() {
+        let lines = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
-        let input = "eightwothree";
-        let output = "8wo3";
-        assert_eq!(parse_numbers(input), output);
-
-        let input = "abcone2threexyz";
-        let output = "abc123xyz";
-        assert_eq!(parse_numbers(input), output);
-
-        let input = "xtwone3four";
-        let output = "x2ne34";
-        assert_eq!(parse_numbers(input), output);
-
-        let input = "4nineeightseven2";
-        let output = "49872";
-        assert_eq!(parse_numbers(input), output);
-
-        let input = "zoneight234";
-        let output = "z1ight234";
-        assert_eq!(parse_numbers(input), output);
-
-        let input = "7pqrstsixteen";
-        let output = "7pqrst6teen";
-        assert_eq!(parse_numbers(input), output);
+        let sum_1: u32 = lines.lines().map(|l| parse_line(l, Regex::new(r"\d").unwrap())).sum();
+        assert_eq!(sum_1, 142)
     }
 
     #[test]
@@ -116,7 +73,7 @@ xtwone3four
 zoneight234
 7pqrstsixteen";
 
-        let total: u32 = lines.lines().map(parse_numbers).map(parse_line).sum();
-        assert_eq!(total, 281);
+        let sum_2: u32 = lines.lines().map(|l| parse_line(l, Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap())).sum();
+        assert_eq!(sum_2, 281);
     }
 }
