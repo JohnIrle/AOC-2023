b365181f5dfd0f175f2b2ac6dd71f999940e5f82
John Irle <irlejohn@gmail.com>
2023-12-04 22:20:17 -0600

    chore: initialize day 5

---
+++ day_05/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn test_part_1() {
+
+    }
+
+    fn test_part_2() {
+
+    }
+}
