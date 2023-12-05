aca208e51443ccbf5103c44ae6f2dbe3a82edbd2
John Irle <irlejohn@gmail.com>
2023-12-04 22:20:59 -0600

    chore: add runner

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,5 +1,24 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("./day_05/part_1.txt").expect("Could Not read file");
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        match args[1].as_str() {
+            "part_1" => {
+            }
+            "part_2" => {
+            }
+            _ => {
+                println!("Usage: <day> <part>");
+                std::process::exit(64);
+            }
+        }
+    } else {
+        println!("Usage: <day> <part>");
+        std::process::exit(64);
+    }
 }
 
 #[cfg(test)]
